1fe7774b324d9c9cfd8e2f40ec5c28da7ca0f292
Mahmudul Hasan Sakib <mahmudulsakib3159@gmail.com>
2025-12-08 22:15:41 +0600

    Defined schema for user table

---
+++ server/src/db/schema.ts
@@ -0,0 +1,20 @@
+import { date, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    email: text("email").unique().notNull(),
+    username: text("username").unique().notNull(),
+    passwordHash: text("password_hash").notNull(),
+    firstName: text("first_name").notNull(),
+    lastName: text("last_name"),
+    bio: text("bio"),
+    phone: text("phone").unique(),
+    avatarUrl: text("avatar_url"),
+    dateOfBirth: date("date_of_birth").notNull(),
+    createdAt: timestamp("created_at", {
+        mode: "date",
+        withTimezone: true,
+    })
+        .notNull()
+        .defaultNow(),
+});
