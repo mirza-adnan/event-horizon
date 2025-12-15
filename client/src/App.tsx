fecd8203caf25bd83679c55b005fa916070ebd48
Mirza Adnan <mirza.adnan2205@gmail.com>
2025-12-15 23:25:53 +0600

    add: client side routing using react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-    return <div className="">Hello Event Horizon</div>;
+    return (
+        <div>
+            <Outlet />
+        </div>
+    );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./global.css";
 import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [],
+    },
+]);
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>
 );
