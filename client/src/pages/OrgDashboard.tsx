b467a944da6ea981d796cedc3cef57cc3e548769
Mubtasim Sajid Ahmed Arian <mubtasimsajidahmedarian.11@gmail.com>
2025-12-20 20:30:28 +0600

    Small fixes

--- client/src/pages/OrgDashboard.tsx
+++ client/src/pages/OrgDashboard.tsx
@@ -41,7 +41,7 @@ function OrgDashboard() {
 					<h2 className="text-2xl font-bold text-white mb-4">
 						Welcome to your Dashboard
 					</h2>
-					{/* Add your main content here */}
+					{/* Main content here */}
 				</div>
 			</div>
 		</div>
