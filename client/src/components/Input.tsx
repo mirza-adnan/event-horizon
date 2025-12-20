c49a76160d975a4891e660a167c670a46fb0bf55
Mirza Adnan <mirza.adnan2205@gmail.com>
2025-12-20 19:50:07 +0600

    add: reusable input component

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -2,44 +2,50 @@ import { type ChangeEvent, type HTMLInputTypeAttribute } from "react";
 import { cn } from "../utils/helpers";
 
 type InputProps = {
-  name?: string;
-  className?: string;
-  type: HTMLInputTypeAttribute;
-  placeholder?: string;
-  required?: boolean;
-  label?: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    name?: string;
+    className?: string;
+    type?: HTMLInputTypeAttribute;
+    placeholder?: string;
+    required?: boolean;
+    label?: string;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    value?: string;
+    error?: string;
 };
 
 function Input({
-  name = "",
-  className,
-  type = "text",
-  placeholder,
-  required = false,
-  label = "",
-  onChange,
+    name = "",
+    className,
+    type = "text",
+    placeholder,
+    required = false,
+    label = "",
+    onChange,
+    value,
+    error,
 }: InputProps) {
-  return (
-    <div className="space-y-2">
-      <label htmlFor="" className="block ml-1">
-        {label}
-        {"   "}
-        {required && <span className="text-danger text-xl">*</span>}
-      </label>
-      <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        className={cn(
-          "p-3 rounded-lg bg-[#141414] text-text-strong w-full border-none outline-none focus:outline-1 focus:outline-accent",
-          className
-        )}
-        required={required}
-        onChange={onChange}
-      />
-    </div>
-  );
+    return (
+        <div className="space-y-2">
+            <label className="block ml-1">
+                {label}
+                {"   "}
+                {required && <span className="text-danger text-xl">*</span>}
+            </label>
+            <input
+                type={type}
+                name={name}
+                value={value}
+                placeholder={placeholder}
+                className={cn(
+                    "w-full px-4 py-2 rounded-lg bg-zinc-800 text-white border-none outline-none ring-1 ring-[#373737] focus:ring-accent",
+                    className
+                )}
+                required={required}
+                onChange={onChange}
+            />
+            {error && <p className="text-danger text-sm mt-1">{error}</p>}
+        </div>
+    );
 }
 
 export default Input;
